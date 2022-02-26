3371f1cf948ec9497a4f8ca671fdb09ba0bd3e0b
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2022-02-26 11:17:08 -0300

    Requerimientos culminados

--- index.js
+++ index.js
@@ -1,51 +1,76 @@
 const inputs = process.argv.slice(2);
 const typeRequest = inputs[0];
-
-const { Client } = require('pg');
-
-const config = {
-  user: 'postgres',
-  host: 'localhost',
-  database: 'escuela',
-  password: '1234',
-  port: 5432,
-};
-
-const client = new Client(config);
+import {
+  newStudent,
+  searchStudent,
+  consulta,
+  editar,
+  eliminar,
+} from './querys.js';
 
 console.log(typeRequest);
 
-async function newStudent(nombre, rut, curso, nivel) {
-  client.connect();
-  try {
-    const res = await client.query(
-      `INSERT INTO estudiantes (nombre, rut, curso, nivel) VALUES ('${nombre}', '${rut}', '${curso}', '${nivel}') RETURNING *`
-    );
-    console.log(`Estudiante ${nombre} agregado con éxito`);
-  } catch (error) {
-    console.error(
-      'Hubo un error: ',
-      error.detail,
-      'Revise los datos a ingresar'
-    );
-  }
-  client.end();
-}
-
-function inputsError(nombre, rut, curso, nivel) {
+////// chequeo de inputs
+function inputsError(
+  nombre = 'nombre valido',
+  rut = '00.000.000-9',
+  curso = 'curso valido',
+  nivel = 9999
+) {
   if (!(nombre && rut && curso && nivel))
     return 'Datos incorrectos o faltantes';
   if (nombre.length > 50) return 'Nombre demasiado largo';
   if (rut.length > 12) return 'Rut no valido';
   if (curso.length > 50) return 'Curso demasiado largo';
+  if (nivel < 1) return 'nivel invalido';
   else return false;
 }
 
-if (typeRequest == 'nuevo') {
-  const [nombre, rut, curso, nivel] = inputs.slice(1);
-  console.log(inputsError(nombre, rut, curso, nivel));
+//ejecuciones
+(async () => {
+  //Nuevo estudiantes
+  if (typeRequest == 'nuevo') {
+    const [nombre, rut, curso, nivel] = inputs.slice(1);
+    const checkInputs = inputsError(nombre, rut, curso, nivel);
+    if (!checkInputs) {
+      const request = await newStudent(nombre, rut, curso, nivel);
+      console.log(request);
+    } else console.log(checkInputs);
+  }
 
-  !inputsError(nombre, rut, curso, nivel)
-    ? inputsError(nombre, rut, curso, nivel)
-    : newStudent(nombre, rut, curso, nivel);
-}
+  // consulta estudiante por rut
+  if (typeRequest == 'rut') {
+    const [rut] = inputs.slice(1);
+    const checkInputs = inputsError('nombre', rut, 'curso', 'nivel');
+    if (!checkInputs) {
+      const request = await searchStudent(rut);
+      console.log(request);
+    } else console.log(checkInputs);
+  }
+
+  // CONSULTA todos los estudiantes
+  if (typeRequest == 'consulta') {
+    const request = await consulta();
+    console.log('Registro actual de estudiantes', request);
+  }
+
+  // EDITAR a un estudiante
+  if (typeRequest == 'editar') {
+    const [nombre, rut, curso, nivel] = inputs.slice(1);
+    const checkInputs = inputsError(nombre, rut, curso, nivel);
+    if (!checkInputs) {
+      const request = await editar(nombre, rut, curso, nivel);
+      console.log(request);
+    } else console.log(checkInputs);
+  }
+
+  // ELIMINAR estudiante por rut
+  if (typeRequest == 'eliminar') {
+    const [rut] = inputs.slice(1);
+    const checkInputs = inputsError('nombre', rut, 'curso', 'nivel');
+    if (!checkInputs) {
+      const request = await eliminar(rut);
+      console.log(request);
+    } else console.log(checkInputs);
+  }
+})();
